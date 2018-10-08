0ec8868398c3a216d86e89dcf805c653d8e7959c
Haoran Fan <fanhr5@gmail.com>
2018-10-07 23:50:13 -0400

    Update .gitignore file, Imported Bootstrap, Completed problem-list and problem-detail components.

---
+++ week1/oj-client/src/app/mock-problems.ts
@@ -0,0 +1,27 @@
+import { Problem } from './models/problem.model';
+
+export const PROBLEMS: Problem[] = [{
+  id: 1,
+  name: 'Problem 1',
+  description: 'This is a description',
+  difficulty: 'easy'
+},
+{
+  id: 2,
+  name: 'Problem 2',
+  description: 'This is the second description',
+  difficulty: 'medium'
+},
+{
+  id: 3,
+  name: 'Problem 3',
+  description: 'This is the third description',
+  difficulty: 'hard'
+},
+{
+  id: 4,
+  name: 'Problem 4',
+  description: 'This is the fourth description',
+  difficulty: 'super'
+}
+];
